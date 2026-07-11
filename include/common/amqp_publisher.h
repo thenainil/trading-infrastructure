1fec9225a47dc5b6479a38f1b8a334362e049e41
Nainil Patel <nainil@fynleaf.com>
2026-07-11 00:43:16 -0400

    AMQP Logging

--- include/common/amqp_publisher.h
+++ include/common/amqp_publisher.h
@@ -8,11 +8,33 @@
 #include <amqpcpp.h>
 #include <amqpcpp/libboostasio.h>
 #include <boost/asio/io_context.hpp>
+#include <iostream>
 #include <string>
 #include <utility>
 
+class LoggingAmqpHandler final : public AMQP::LibBoostAsioHandler {
+public:
+    explicit LoggingAmqpHandler(boost::asio::io_context& io_context) :
+        AMQP::LibBoostAsioHandler(io_context) {}
+
+    void onReady(AMQP::TcpConnection* connection) override {
+        (void) connection;
+        std::cerr << "RabbitMQ connection ready\n";
+    }
+
+    void onError(AMQP::TcpConnection* connection, const char* message) override {
+        (void) connection;
+        std::cerr << "RabbitMQ connection error: " << message << '\n';
+    }
+
+    void onClosed(AMQP::TcpConnection* connection) override {
+        (void) connection;
+        std::cerr << "RabbitMQ connection closed\n";
+    }
+};
+
 class AmqpPublisher {
-    AMQP::LibBoostAsioHandler handler;
+    LoggingAmqpHandler handler;
     AMQP::TcpConnection connection;
     AMQP::TcpChannel channel;
     std::string routing_key;
@@ -22,10 +44,19 @@ public:
         handler(io_context),
         connection(&handler, AMQP::Address(amqpUrl)),
         channel(&connection),
-        routing_key(std::move(queueName)) {}
+        routing_key(std::move(queueName)) {
+        channel.onReady([this] {
+            std::cerr << "RabbitMQ channel ready for queue/routing key: " << routing_key << '\n';
+        });
+        channel.onError([](const char* message) {
+            std::cerr << "RabbitMQ channel error: " << message << '\n';
+        });
+    }
 
     void publishMessage(const std::string& message) {
-        channel.publish("", routing_key, message);
+        if (!channel.publish("", routing_key, message)) {
+            std::cerr << "RabbitMQ publish was not accepted by the client channel\n";
+        }
     }
 };
 
